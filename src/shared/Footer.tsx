ae6c54ddb167fe13c625d1946c81a9f7c050c6e4
Lee Blundell <leeblundell16@gmail.com>
2019-12-29 12:45:22 +0000

    Update footer

--- src/shared/Footer.tsx
+++ src/shared/Footer.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  background-color: #e5e5e5;
+  background-color: #fa4047;
+  opacity: 0.9;
 `
 
 const Footer = () =>
   <StyledFooter className="p-3">
-    <div className="container">
+    <div className="container text-light">
     Redbridge Motor Company Ltd.
     </div>
   </StyledFooter>
